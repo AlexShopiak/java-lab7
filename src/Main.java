b5c3eb994d4f47f1337de96aa8716dc24c6dae71
Alex Shopiak <alshop2004@gmail.com>
2023-12-22 11:08:05 +0000

    Add entry point

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Main
+ *
+ * Version 1.0
+ *
+ * (c) 2023 Oleksii Shopiak
+ * All rights reserved.
+ *
+ * This is an entry point of a project
+ */
+import collection.StoneSet;
+
+public class Main {
+    public static void main(String[] args) {
+        StoneSet set = new StoneSet();
+    }
+}
