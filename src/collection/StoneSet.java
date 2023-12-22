1a6efc70d7b78c93be2e53cb40d2d69041b13c66
Alex Shopiak <alshop2004@gmail.com>
2023-12-22 11:10:21 +0000

    Add StoneSet structure

---
+++ src/collection/StoneSet.java
@@ -0,0 +1,95 @@
+/*
+ * StoneSet
+ *
+ * Version 1.0
+ *
+ * (c) 2023 Oleksii Shopiak
+ * All rights reserved.
+ *
+ * It is a typed set of Stone class ojects 
+ * which has doubly linked array structure
+ */
+package collection;
+
+import java.util.Collection;
+import java.util.Iterator;
+import stone.Stone;
+
+public class StoneSet extends DoublyLinkedList{ 
+    public StoneSet() {
+        super();
+        //todo
+    }
+
+    public StoneSet(Stone element) {
+        super();
+        //todo
+    }
+
+    public StoneSet(Collection<Stone> collection) {
+        super();
+        //todo
+    }
+
+    public boolean add(Stone element) {
+        return false;
+        //todo
+    }
+
+    public boolean addAll(Collection<Stone> collection) {
+        return false;
+        //todo
+    }
+
+    public void clear() {}
+
+    public boolean contains(Stone element) {
+        return false;
+        //todo
+    }
+
+    public boolean containsAll(Collection<Stone> collection) {
+        return false;
+        //todo
+    }
+
+    public int hashCode() {
+        return 0;
+        //todo
+    }
+
+    public boolean isEmpty() {
+        return false;
+        //todo
+    }
+
+    public Iterator iterator() {
+        return iterator();
+        //todo
+    }
+
+    public boolean remove(Stone element) {
+        return false;
+        //todo
+    }
+
+    public boolean removeAll(Collection<Stone> collection) {
+        return false;
+        //todo
+    }
+
+    public boolean retainAll(Collection<Stone> collection) {
+        return false;
+        //todo
+    }
+
+    public int size() {
+        return 0;
+        //todo
+    }
+
+    public Object[] toArray() {
+        return new Object[0];
+        //todo
+    }
+}
